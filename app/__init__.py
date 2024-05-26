acb6dfe6075522db2a219f401c0495644efe9469
D5F <audaciusadm@gmail.com>
2024-05-26 20:12:27 -0300

    add all

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
-import os
 
 def create_app():
-    app = Flask(__name__, template_folder=os.path.abspath('templates'))
+    app = Flask(__name__)
     
-    with app.app_context():
-        # Import routes
-        from . import routes
-        
-        return app
+    from .routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
