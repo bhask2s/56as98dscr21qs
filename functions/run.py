09d2100205a3ea8bdb3b2fa0f3696436a2fce568
D5F <audaciusadm@gmail.com>
2024-05-26 20:58:47 -0300

    add all

--- functions/run.py
+++ functions/run.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 from app import create_app
+
+load_dotenv()
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888)
+if __name__ == "__main__":
+    env = os.getenv("FLASK_ENV", "production")
+    app.run(host='0.0.0.0', port=8888, debug=(env == "development"))
